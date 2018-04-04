aa5da425ebdd4e2af3680b6eb8f2f90550c2570e
Romain Trachel <trachel.romain@ironova.com>
2018-04-04 17:53:27 +0200

    WIP: added word sequence parsing

--- load_dataset.py
+++ load_dataset.py
@@ -1,18 +1,36 @@
-import xml.etree.ElementTree as etree
-data = []
-for event, elem in etree.iterparse('EF201403_selection121.xml', events=('start', 'end', 'start-ns', 'end-ns')):
-    if event == 'start':
-        if elem.tag == 'writing':
-            new_sample = dict(elem.items())
-            for subelem in elem.getchildren():
-                if subelem.tag == 'learner':
-                    for key, val in subelem.items():
-                        if key == 'id':
-                            new_sample['learn_id'] = val
-                elif subelem.tag == 'grade':
-                    if subelem.text is not None:
-                        new_sample['grade'] = float(subelem.text)
-                        continue
-                elif subelem.tag == 'text':
-                    new_sample['text'] = subelem.text
-            data.append(new_sample)
+from keras.preprocessing.text import one_hot, text_to_word_sequence
+from keras.preprocessing import sequence
+
+#import xml.etree.ElementTree as etree
+from lxml import etree
+sentences, grades, levels, learners, indexes = [], [], [], [], []
+for event, elem in etree.iterparse('EF201403_selection121.xml',
+                                    html=True, events=('start', 'start-ns')):
+    if elem.tag == 'writing':
+        elems = dict(elem.items())
+        level = int(elems['level'])
+        sample_id = int(elems['id'])
+        print('Parsing sample', sample_id)
+        for subelem in elem.getchildren():
+            if subelem.tag == 'learner':
+                for key, val in subelem.items():
+                    if key == 'id':
+                        learn_id = int(val)
+            elif subelem.tag == 'grade':
+                if subelem.text is not None:
+                    grade = float(subelem.text)
+                else:
+                    continue
+            elif subelem.tag == 'text':
+                if subelem.text is not None:
+                    # convert the text and filter out characters
+                    text_seq = text_to_word_sequence(subelem.text)
+                else:
+                    continue
+
+        # append data if parsing went well...
+        sentences.append(text_seq)
+        grades.append(grade)
+        levels.append(level)
+        learners.append(learn_id)
+        indexes.append(sample_id)
