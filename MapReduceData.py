9014c6987c392e28305bed7b008263b5d786601b
Unknown <louche.ugo@gmail.com>
2018-04-10 21:44:45 +0200

    MapReduce to Dataset primitives

--- MapReduceData.py
+++ MapReduceData.py
@@ -40,7 +40,7 @@ class MapReduceData:
     
     
     def groupByKey(self, key):
-        if self.depth > 1:
+        if self.depth > 0:
             ret_data = dict((k, v.groupByKey(key)) for k, v in self.data.items())
             return MapReduceData(ret_data, self.depth+1)
         else:
@@ -54,9 +54,72 @@ class MapReduceData:
             for k in di:
                 di[k] = MapReduceData(di[k])
             return MapReduceData(di, 1)
+        
+    
+    def filterKeys(self, keyList=[]):
+        keyList = typeCast.listify(keyList)
+        if len(keyList) == 0:
+            return self
+        
+        if self.depth > 0:
+            ret_data = dict((k, v.filterKeys(keyList))  for k, v in self.data.items())
+            return MapReduceData(ret_data, self.depth)
+        else:
+            return MapReduceData(map(lambda x: dict((k, v) for k, v in x.items() if k in keyList),
+                                     self.data))
     
     
     def getDataSet(self, keyList=[]):
+        if len(keyList) != 0:
+            return self.filterKeys(keyList).getDataSet()
+        
+        if self.depth > 0:
+            ret = []
+            for v in self.data.values():
+                fn, ds = v.getDataSet()
+                ret.append(ds)
+            ret = np.vstack(ret)
+            return fn, ret
+        else:
+            self.flatten()
+            fn = [k for k in self.data[0]]
+            ret = [list(sample.values()) for sample in self.data]
+            ret = np.array(ret)
+            return fn, ret
+    
+    
+    #Only work with nested list, other nest container (e.g. dict, set) won't be affected
+    def flatten(self):
+        if self.depth > 1:
+            for k in self.data:
+                self.data[k].flatten()
+            return
+        else:
+            self._flatten_subroutine()
+            return
+    
+    def _flatten_subroutine(self):
+        reloop=True
+        while(reloop): #If a flatten happen, perform a recheck as more nested could have been uncovered
+            reloop=False
+            #Loop over all entries
+            for d in self.data:
+                to_add = []
+                to_remove = []
+                #Loop over the keys of an entry and check what to remove/add
+                for k in d:
+                    if type(d[k]) == type([]):
+                        reloop=True
+                        to_remove.append(k)
+                        for idx, x in enumerate(d[k]):
+                            to_add.append((k+'_{}'.format(idx)), x)
+                #Add and remove keys (you can't do this while iterating over the dictionary)
+                for k, v in to_add:
+                    d[k] = v
+                for k in to_remove:
+                    d.pop(k)
+                    
+        
         
             
         
