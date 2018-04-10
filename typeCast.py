d60fd6aed3858cb18ca795cc78b61eac1195be38
Unknown <louche.ugo@gmail.com>
2018-04-10 19:12:54 +0200

    listify

--- typeCast.py
+++ typeCast.py
@@ -4,4 +4,12 @@
 def update_return(a, b):
     a.update(b)
     return a
+
+
+#All-time favorite listify
+def listify(x):
+    if type(x) == type([]):
+        return x
+    else:
+        return [x]
     
