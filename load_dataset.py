baf311f360b86f504109e8f68bbfb5a205246fec
Unknown <louche.ugo@gmail.com>
2018-04-11 23:36:32 +0200

    Cleanup and streamlined XMLparse method.
    Allow for robust (slow, memory efficient ?) or non robust (fast xml parsing

--- load_dataset.py
+++ load_dataset.py
@@ -1,10 +1,9 @@
 import platform
-import datetime
 import os
 
-import typeCast
-import MapReduceData
+import parse_xml
 
+import MapReduceData as mrd
 import numpy as np
 import pickle as pkl
 
@@ -24,137 +23,25 @@ from keras.preprocessing.text import one_hot, text_to_word_sequence
 from sklearn.cross_validation import train_test_split
 
 
-#Default function for parsing Cambridge data
-def defaultParserFunction_Cambridge(path):
-    #Only get to the writings tag and process childs from here
-    for event, elem in etree.iterparse(path):
-        if elem.tag=='writings':
-            break 
-    print('Writings tag found with {} children(s)'.format(len(elem.getchildren())))
-    
-    ##First Pass
-    print('Starting first pass')
-    skipped = 0
-    nbEntries = 0
-    data = []
-    report_freq = int(0.1 * len(elem.getchildren()))
-    for idEntry, entry in enumerate(elem.getchildren()):
-        if entry.tag != 'writing':
-            print('Not a writing a entry ({} found). Skip'.format(entry.tag))
-            skipped+=1
-            continue
-        #All writing entry should follow the same hierarchy
-        local = {}
-        attr = dict(entry.items())
-        local['entry_id'] = attr['id']
-        local['level'] = attr['level']
-        local['unit'] = attr['unit']
-        
-        #Process child entries (text, learner, topic, date and grade)
-        if len(entry.getchildren()) != 5:
-            print('Entry with invalid number of children (expected 5, found {}). skipped'\
-                  .format(len(entry.getchildren())))
-            skipped+=1
-            continue
-        for child in entry.getchildren():
-            attr = dict(child.items())
-            
-            #Parse Learner tag
-            if child.tag == 'learner':
-                local['learner_id'] = int(attr['id'])
-                local['nationality'] = attr['nationality']
-            
-            #Parse topic tag
-            elif child.tag == 'topic':
-                local['topic_id'] = int(attr['id'])
-            
-            #Parse date tag
-            elif child.tag == 'date':
-                if child.text is None:
-                    print('Entry has no date. Skipped')
-                    skipped+=1
-                    continue
-                try:
-                   date = datetime.datetime.strptime(child.text[0:19], '%Y-%m-%d %H:%M:%S')
-                except:
-                    print('Invalid date. Skipped')
-                    skipped +=1
-                    continue
-                local['year']=date.year
-                local['month']=date.month
-                local['day']=date.day
-                local['hour']=date.hour
-                local['minute']=date.minute
-                local['second']=date.second
-            
-            #Parse grade tag
-            elif child.tag == 'grade':
-                if child.text is None:
-                    print('Entry has no grade. skipped')
-                    skipped+=1
-                    continue
-                local['grade'] = float(child.text)
-            
-            #Parse text tag
-            elif child.tag == 'text':
-                if child.text is None:
-                    print('Entry has no text. skipped')
-                    skipped+=1
-                    continue
-                local['text'] = text_to_word_sequence(child.text)
-        
-        #Parsing ok
-        data.append(local)
-        nbEntries+=1
-        if idEntry % report_freq == 0:
-            print('{} done over {} total ({:.2f}%)'\
-                  .format(idEntry, len(elem.getchildren()), (idEntry/len(elem.getchildren())*100)))
-    #First pass done
-    print('First Pass done')
-    print('{} entries collected, {} skipped'.format(nbEntries, skipped))
-    
-    ##Second pass, text formatting
-    print('Aggregating text data')
-    text_len = list(map(lambda d: len(d['text']), data))
-    text_len = np.array(text_len)
-    max_len = int(text_len.mean()+3*text_len.std())
-    print('Average text length is: {:.2f} (std: {:.2f}) setting MAX_LEN to {}'\
-          .format(text_len.mean(), text_len.std(), max_len))
-    
-    lexicon = reduce(typeCast.update_return, [set()] + list(map(lambda d: d['text'], data)))
-    lexicon = list(lexicon)
-    word_indices = dict((w, idx) for idx, w in enumerate(lexicon))
-    print('Lexicon size is {}'.format(len(lexicon)))
-    
-    print('Formatting text data')
-    report_freq = int(0.1 * len(data))
-    for idData, d in enumerate(data):
-        d['text'] = list(map(lambda w: word_indices[w], d['text']))
-        d['text'] = sequence.pad_sequences([d['text']], maxlen=max_len)[0]
-        
-        if idData % report_freq == 0:
-            print('{} done over {} total ({:.2f}%)'\
-                  .format(idData, len(data), (idData/len(data)*100)))
-            
-    #All done
-    return data
+
 
 if __name__ == '__main__':
     #Use platform as os.uname() does not exist on Windows
     if platform.uname()[1] == 'DESKTOP-42C7TJ2':
-        file_path = 'data/EF201403_selection59.xml'
+        file_path = 'data/Cambridge_faulty.xml'
         parsed_path = 'data/Cambridge_parsed.pkl'
     else:
         file_path = 'EF201403_selection121.xml'
         parsed_path = '' #TODO set a properparsed path
         
     if not os.path.isfile(parsed_path):
-        data = defaultParserFunction_Cambridge(file_path)
-        pkl.dump(data, open(parsed_path, 'wb'))
+        data = parse_xml.parseXML(file_path, True)
+        #pkl.dump(data, open(parsed_path, 'wb'))
     else:
-        data = pkl.load(open(parsed_path, 'rb'))
+        pass
+        #data = pkl.load(open(parsed_path, 'rb'))
     
-    data = MapReduceData(data)
+    data = mrd.MapReduceData(data)
         
     
     
---
+++ parse_xml.py
@@ -0,0 +1,158 @@
+import datetime
+
+import typeCast
+
+import numpy as np
+
+from lxml import etree
+from functools import reduce
+
+from keras.preprocessing import sequence
+from keras.preprocessing.text import text_to_word_sequence
+
+
+def parseWritingTag(writing):
+    #All writing entry should follow the same hierarchy
+    ret = {}
+    attr = dict(writing.items())
+    ret['entry_id'] = attr['id']
+    ret['level'] = attr['level']
+    ret['unit'] = attr['unit']
+    
+    #Process child entries (text, learner, topic, date and grade)
+    if len(writing.getchildren()) != 5:
+        print('Entry with invalid number of children (expected 5, found {}). skipped'\
+              .format(len(ret.getchildren())))
+        return None
+    
+    for child in writing.getchildren():
+        attr = dict(child.items())
+        
+        #Parse Learner tag
+        if child.tag == 'learner':
+            ret['learner_id'] = int(attr['id'])
+            ret['nationality'] = attr['nationality']
+        
+        #Parse topic tag
+        elif child.tag == 'topic':
+            ret['topic_id'] = int(attr['id'])
+        
+        #Parse date tag
+        elif child.tag == 'date':
+            if child.text is None:
+                print('Entry has no date. Skipped')
+                return None
+            try:
+               date = datetime.datetime.strptime(child.text[0:19], '%Y-%m-%d %H:%M:%S')
+            except:
+                print('Invalid date. Skipped')
+                return None
+            ret['year']=date.year
+            ret['month']=date.month
+            ret['day']=date.day
+            ret['hour']=date.hour
+            ret['minute']=date.minute
+            ret['second']=date.second
+        
+        #Parse grade tag
+        elif child.tag == 'grade':
+            if child.text is None:
+                print('Entry has no grade. skipped')
+                return None
+            ret['grade'] = float(child.text)
+        
+        #Parse text tag
+        elif child.tag == 'text':
+            if child.text is None:
+                print('Entry has no text. skipped')
+                return None
+            ret['text'] = text_to_word_sequence(child.text)
+            
+    return ret
+    
+
+def formatText(data):
+    ##Second pass, text formatting
+    print('Aggregating text data')
+    text_len = list(map(lambda d: len(d['text']), data))
+    text_len = np.array(text_len)
+    max_len = int(text_len.mean()+3*text_len.std())
+    print('Average text length is: {:.2f} (std: {:.2f}, max: {:.2f}, min{:.2f}) setting MAX_LEN to {}'\
+          .format(text_len.mean(), text_len.std(), text_len.max(), text_len.min(), max_len))
+    
+    lexicon = reduce(typeCast.update_return, [set()] + list(map(lambda d: d['text'], data)))
+    lexicon = list(lexicon)
+    word_indices = dict((w, idx) for idx, w in enumerate(lexicon))
+    print('Lexicon size is {}'.format(len(lexicon)))
+    
+    print('Formatting text data')
+    report_freq = int(0.1 * len(data))
+    for idData, d in enumerate(data):
+        d['text'] = list(map(lambda w: word_indices[w], d['text']))
+        d['text'] = sequence.pad_sequences([d['text']], maxlen=max_len)[0]
+        
+        if idData % report_freq == 0:
+            print('{} done over {} total ({:.2f}%)'\
+                  .format(idData, len(data), (idData/len(data)*100)))
+            
+    #All done
+    return data
+    
+
+#Default function for parsing Cambridge data
+#Will either look for writing tags in XML tree or writings (notice the 's')
+#Looking for writings should be faster but if there are parsing errors, some entries
+#might be missing.
+def parseXML(path, robust=False):
+    print('Starting first pass')
+    print('Robust is {}'.format(robust))
+    if robust: 
+        html_arg=True
+        print('Will parse writing tags in child and progress will not show')
+    else: 
+        html_arg=False
+        print('Will parse childs of writings tag but some entries might be skipped')
+        
+    skipped = 0
+    nbEntries = 0
+    data = []
+    for event, elem in etree.iterparse(path, html=html_arg):
+        #Look only for the 'writings' tag and break
+        if (not robust) and elem.tag=='writings':
+            report_freq = int(0.1 * len(elem.getchildren()))
+            for idEntry, entry in enumerate(elem.getchildren()):
+                if entry.tag != 'writing':
+                    print('Not a writing a entry ({} found). Skip'.format(entry.tag))
+                    skipped+=1
+                    continue
+                #Legit entry
+                local = parseWritingTag(entry)
+                if local is None:
+                    skipped+=1
+                else:
+                    #Parsing ok
+                    data.append(local)
+                    nbEntries+=1
+                #Print progress    
+                if idEntry % report_freq == 0:
+                    print('{} done over {} total ({:.2f}%)'\
+                          .format(idEntry, len(elem.getchildren()), 
+                                  (idEntry/len(elem.getchildren())*100)))
+            break
+        #Look for every 'writing tag'
+        elif robust and elem.tag=='writing':
+            local = parseWritingTag(elem)
+            if local is None:
+                skipped+=1
+            else:
+                #Parsing ok
+                data.append(local)
+                nbEntries+=1
+            
+    #First pass done
+    print('First Pass done')
+    print('{} entries collected, {} skipped'.format(nbEntries, skipped))
+    
+    return formatText(data)
+    
+ 
